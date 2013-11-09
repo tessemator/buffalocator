64cd03995ecc21e3fbd3e46c7e541a7accd6158c
Lindsey Tessmer <tessemator@gmail.com>
2013-11-08 20:36:39 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuffalocatorV1::Application.config.secret_key_base = '232fbcac40b760f4a7d593c9cdc4af9a8115d2a8031cf1265cbbb59928a199bc15fec4a36116120726b46a67c10a1894ecbd60c7e3126a1dece78f734c6cb908'
